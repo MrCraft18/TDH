0687324a0b95c163f354fcde39585694f210dbd3
MrCraft18 <mariocaden@gmail.com>
2023-10-24 11:34:21 -0500

    Push

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ fs = require('fs')
 
 const list = fs.readdirSync("./Mock Order List")
 
+list.sort((a, b) => {
+    const numA = parseInt(a.split('_')[0], 10);
+    const numB = parseInt(b.split('_')[0], 10);
+    return numA - numB;
+  });
+
 console.log(list)
