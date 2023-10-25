2b242c3654069553b29424bdb2acc3d13ab96c6f
MrCraft18 <mariocaden@gmail.com>
2023-10-25 17:28:14 -0500

    Netbook

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { readFileSync } = require('fs');
+
 fs = require('fs')
 
 const list = fs.readdirSync("./Mock Order List")
@@ -6,6 +8,15 @@ list.sort((a, b) => {
     const numA = parseInt(a.split('_')[0], 10);
     const numB = parseInt(b.split('_')[0], 10);
     return numA - numB;
-  });
+})
 
-console.log(list)
+
+
+const orders = []
+list.forEach(order => {
+    const orderJSON = JSON.parse(readFileSync(`./Mock Order List/${order}`))
+
+    orders.push(orderJSON)
+})
+
+console.log(JSON.stringify(orders, null, 4))
