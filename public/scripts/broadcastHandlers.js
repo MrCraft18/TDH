cc98b04a61ca9fcc0af067f79ea124633722d630
MrCraft18 <mariocaden@gmail.com>
2023-10-31 12:06:36 -0500

    Netbook

--- public/scripts/broadcastHandlers.js
+++ public/scripts/broadcastHandlers.js
@@ -1,17 +1,27 @@
 socket.on('editOrderBroadcast', body => {
     try {
+        const order = body.order
+
         const jsonElements = [...document.querySelectorAll('.json')]
         const jsonElement = jsonElements.find(element => {
             const orderJSON = JSON.parse(element.innerText)
-            return (orderJSON.serial == body.order.serial) 
+            return (orderJSON.serial == order.serial) 
         })
 
-        const orderElement = jsonElement.closest('.order')
-        const changedParameter = convertCase(body.changedParameter, 'kebab')
+        const orderDiv = jsonElement.closest('.order')
+        const selectElements = orderDiv.querySelectorAll('.custom-select');
+        const dateElements = orderDiv.querySelectorAll('input[type="date"]')
+        const inputElements = [...selectElements, ...dateElements]
 
-        if (changedParameter === 'order-status') {
-            const changedElement = orderElement.querySelector('.order-status')
-        }
+        inputElements.forEach(element => {
+            const parameter = convertCase(getParameter(element.classList), camel)
+
+            if (element.closest('.parts')) {
+
+            } else {
+                element.value = order[parameter]
+            }
+        })
     } catch (err) {
         console.log(err)
         console.log(`Error Editing Order from Broadcast`)
